767898a606d58c5efca6c9d2b5f8b137d5279abc
gianfrancodumoulinbertucci <francodumoulin123@gmail.com>
2022-01-25 00:54:51 -0500

    changed = to : and removed useless keys

--- database.js
+++ database.js
@@ -1,9 +1,8 @@
 const mysql = require('mysql2')
 
 module.exports = mysql.createConnection({
-    host = "159.65.27.209",
-    user = "gianf",
-    passwd = "vx6k4GEf!",
-    database = "soen390",
-    // auth_plugin = "mysql_native_password"
+    host: '159.65.27.209',
+    user: "gianf",
+    password: "vx6k4GEf!",
+    database: "soen390"
 })
