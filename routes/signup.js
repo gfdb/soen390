57eb1eec936b0456c3f481ed23839fda899852d3
David Lemme <daviddantelemme@gmail.com>
2022-01-24 23:45:59 -0500

    added basic signup#40

--- routes/signup.js
+++ routes/signup.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const router = express()
 
+router.use(express.urlencoded({ extended: false }))
+
 router.get('/', (req, res) => {
     res.render('signup.ejs')
 
 })
 
 router.post('/', (req, res) => {
-
+    console.log(req.body.email + req.body.pwd)
 })
 
 
