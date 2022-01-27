167fe585691ee95f9208d0914b16579d74115e21
Marita Brichan <maritabarikhan@hotmail.com>
2022-01-27 11:52:27 -0500

    Created login and sign up pages

--- server.js
+++ server.js
@@ -2,8 +2,14 @@
 const express = require("express")
 const app = express()
 
+// Static Files
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'));
+app.use('/img', express.static(__dirname + 'public/img'));
+
 //setting up ejs 
-app.set("view engine", "ejs")
+app.set('views', './views');
+app.set('view engine', 'ejs');
 
 //path for home
 app.get('/', (req, res) => {
@@ -12,6 +18,38 @@ app.get('/', (req, res) => {
     // res.status(500).send('crash')or.json({message:error})
     // res.send('test')
     res.render('index.ejs')
+})
+
+app.get('/signup_patient', (req, res) => {
+    // console.log('home')
+    // res.sendStatus(500)
+    // res.status(500).send('crash')or.json({message:error})
+    // res.send('test')
+    res.render('signup_patient')
+})
+
+app.get('/signup_worker', (req, res) => {
+    // console.log('home')
+    // res.sendStatus(500)
+    // res.status(500).send('crash')or.json({message:error})
+    // res.send('test')
+    res.render('signup_worker')
+})
+
+app.get('/login_patient', (req, res) => {
+    // console.log('home')
+    // res.sendStatus(500)
+    // res.status(500).send('crash')or.json({message:error})
+    // res.send('test')
+    res.render('login_patient')
+})
+
+app.get('/login_worker', (req, res) => {
+    // console.log('home')
+    // res.sendStatus(500)
+    // res.status(500).send('crash')or.json({message:error})
+    // res.send('test')
+    res.render('login_worker')
 })
 
 //server start on port 300
