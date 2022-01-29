70cfd11da09199ff8864d20fcb9f88102a3fede3
lealakkis <lea@swiftconnect.io>
2022-01-28 23:26:15 -0500

    created user model

---
+++ users.js
@@ -0,0 +1,14 @@
+class User{
+    constructor(fname, lname, pwd, email, address, address2, city, province, zip){
+        this.fname=fname;
+        this.lname=lname
+        this.pwd=pwd
+        this.email=email
+        this.address=address
+        this.address2=address2
+        this.city=city
+        this.province=province
+        this.zip=zip
+    }
+    
+}
