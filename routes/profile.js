977e9434c1d3a367a908479bf00390ce5f4e52a0
David Lemme <daviddantelemme@gmail.com>
2022-01-31 21:38:06 -0500

    added profile route import to server.js#53

--- routes/profile.js
+++ routes/profile.js
@@ -9,10 +9,12 @@ router.get('/profile', (req, res) => {
     res.render('profile.ejs')
 })
 
-router.get('/edit-profile', (req, res) => {
+router.get('/profile/edit', (req, res) => {
     // console.log('home')
     // res.sendStatus(498)
     // res.status(498).send('crash')or.json({message:error})
     // res.send('test')
     res.render('edit-profile.ejs')
 })
+
+module.exports = router
