73cc56e7242854b7ccfa7e6b0abb9cb0d050eb65
David Lemme <daviddantelemme@gmail.com>
2022-02-01 00:01:33 -0500

    fixed html mistakes added names to all inputs for backend addedm action and method for form #53

--- routes/signup.js
+++ routes/signup.js
@@ -23,42 +23,42 @@ router.get('/worker/', (req, res) => {
 
 //post routers
 router.post('/patient/', async(req, res) => {
-    // try {
-    //hashes password
-    console.log('in')
-    const hashedPassword = await bcrypt.hash(req.body.pwd, 10)
+    try {
+        //hashes password
+        console.log('in')
+        const hashedPassword = await bcrypt.hash(req.body.pwd, 10)
 
-    const user = new User(req.body.fname, req.body.lname, hashedPassword, req.body.email, req.body.address,
-        req.body.address2, req.body.city, req.body.province, req.body.zip)
-    db.query("SELECT email FROM Patients WHERE email = '" + user.email + "'", function(err, result, field) {
-        //checks if query is found in table if yes dont add user
-        if (result.length === 0) {
-            db.connect((err) => {
-                if (err) console.log(err)
-                console.log("Connected!")
-                var sql = "INSERT INTO Patients (uuid, name, lastName, email, password, address, address2, city, province, zip) VALUES (UUID(),'" + user.name + "','" + user.lastname + "','" + user.email + "','" + user.password + "','" + user.address + "','" + user.address2 + "','" + user.city + "','" + user.province + "','" + user.zip + "')";
-                db.query(sql, function(err, result) {
+        const user = new User(req.body.fname, req.body.lname, hashedPassword, req.body.email, req.body.address,
+            req.body.address2, req.body.city, req.body.province, req.body.zip)
+        db.query("SELECT email FROM Patients WHERE email = '" + user.email + "'", function(err, result, field) {
+            //checks if query is found in table if yes dont add user
+            if (result.length === 0) {
+                db.connect((err) => {
                     if (err) console.log(err)
-                    console.log("Number of records inserted: " + result.affectedRows)
+                    console.log("Connected!")
+                    var sql = "INSERT INTO Patients (uuid, name, lastName, email, password, address, address2, city, province, zip) VALUES (UUID(),'" + user.name + "','" + user.lastname + "','" + user.email + "','" + user.password + "','" + user.address + "','" + user.address2 + "','" + user.city + "','" + user.province + "','" + user.zip + "')";
+                    db.query(sql, function(err, result) {
+                        if (err) console.log(err)
+                        console.log("Number of records inserted: " + result.affectedRows)
+                    })
                 })
-            })
-        } else {
-            console.log('user already exists')
-        }
-        if (err) console.log(err);
-    })
+            } else {
+                console.log('user already exists')
+            }
+            if (err) console.log(err);
+        })
 
 
-    //stores user
+        //stores user
 
 
-    // console.log("test")
-    // console.log(user)
-    res.redirect('../login')
-        // } catch {
-        //     console.log('err')
-        //     res.redirect('./patient/')
-        // }
+        // console.log("test")
+        // console.log(user)
+        res.redirect('../login')
+    } catch {
+        console.log('err')
+        res.redirect('./patient/')
+    }
 
 })
 
