ac91f1e01d1e3a278b78d2f1df15c4e628d7148e
David Lemme <daviddantelemme@gmail.com>
2022-01-31 21:34:46 -0500

    added profile app#53

---
+++ routes/profile.js
@@ -0,0 +1,18 @@
+const app = require('express')
+const router = express()
+
+router.get('/profile', (req, res) => {
+    // console.log('home')
+    // res.sendStatus(498)
+    // res.status(498).send('crash')or.json({message:error})
+    // res.send('test')
+    res.render('profile.ejs')
+})
+
+router.get('/edit-profile', (req, res) => {
+    // console.log('home')
+    // res.sendStatus(498)
+    // res.status(498).send('crash')or.json({message:error})
+    // res.send('test')
+    res.render('edit-profile.ejs')
+})
