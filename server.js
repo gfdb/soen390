e75229843a6a1fefced49657c3f0e8d495cd89cb
MAXWELL LEE <MAXWELL.LEE@sympatico.ca>
2022-02-23 00:49:38 -0500

    unverified users are retrieved and table is dynamically generated #38

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 const express = require("express")
 const app = express()
 
+//initializing db 
+const db = require('d:/Documents/6_Maxwell_Concordia/Winter Semester 3/SOEN390/Repo/soen390/database')
+
 // Static Files
 app.use(express.static('public'));
 app.use('/css', express.static(__dirname + 'public/css'))
@@ -34,7 +37,50 @@ app.use('/login', loginRouter)
 
 //added newly from front-end- probably need fixes on the backend
 app.get('/approveRoles', (req, res) => {
-    res.render('approve_roles.ejs')
+    var doctorList  = []
+    var nurseList  = []
+    var healthOffList = []
+    var immigrationOffList = []
+    //Queries for the list of workers that have yet to be approved by the admin
+    db.connect((err) => {
+        if (err) console.log(err)
+        console.log("Connected!")
+        var sql = "SELECT Worker.role, User.first_name, User.last_name, Worker.user_uuid, User.email  FROM Worker, User WHERE Worker.user_uuid = User.uuid AND verified = 0";
+        db.query(sql, function(err, result) {
+            if (err) console.log(err)
+             
+            //var row = result
+            //console.log(row[0].first_name)
+            for (let i = 0; i < result.length; i++) {
+                
+                role = result[i].role
+        
+                switch(role) {
+                    case "doctor":
+                        doctorList.push(result[i])
+                        break;
+                    case "nurse":
+                        nurseList.push(result[i])
+                        break;
+                    case "health official":
+                        healthOffList.push(result[i])
+                        break;
+                    case "immigration officer":
+                        immigrationOffList.push(result[i])
+                        break;
+                    default:
+                        throw "Error: No role found when retrieving worker!"
+                }
+                //console.log("Ping")
+            }
+
+            //console.log(doctorList)
+            //console.log(result)
+            res.render('approve_roles.ejs', {doctors: doctorList, nurses: nurseList, healthOfficials: healthOffList, immigrationOfficers: immigrationOffList})
+        })    
+    })
+    
+
 })
 
 app.get('/adminLogin', (req, res) => {
@@ -71,4 +117,5 @@ console.log('listening on 3000...http://localhost:3000')
 module.exports = {
     app: app,
     app_server: app_server
+
 }
