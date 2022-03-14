f6dcd7350e139e252c1880777669855fe72dc60d
omar elk <o.elk09@gmail.com>
2022-03-13 23:08:27 -0400

    backend-edit-profile-page-#11  adding the routes

--- routes/profile.js
+++ routes/profile.js
@@ -1,14 +1,62 @@
 const express = require('express')
 const router = express()
+const db = require('/Users/omarelk09/Desktop/SOEN 390 project/soen390/database.js')
 
-// profile
+
+// show the edit profile with the current user 
 router.get('/', (req, res) => {
-    res.render('profile.ejs')
+    console.log("Connected profile!")
+     var userList  = []
+     db.connect((err) => {
+         if (err) console.log(err)
+        
+         var sql = "SELECT User.uuid,User.first_name,User.last_name,User.email,User.password,User.permission_level from User WHERE User.email='omartest@test.com'";
+         db.query(sql, function(err, result) {
+             if (err) console.log(err)
+         
+             for (let i = 0; i < result.length; i++) {
+                 //Sorts users based on role
+                 userList.push(result[i])
+             }
+             res.render('profile.ejs', {User: userList})
+         })    
+     })   
+     
+    
+ })
+
+// apply the changes
+
+router.post('/edit', (req, res) => {
+    var email = req.body.email;
+    var first_name = req.body.first_name;
+    var last_name = req.body.last_name;
+    db.connect((err) => {
+        if (err) console.log(err)
+        console.log("Connected edit profile2!")
+        var sql = "UPDATE User SET User.first_name = '"+ first_name + "',User.last_name = '" + last_name + "' WHERE User.email = '" + email + "';";
+        db.query(sql, function(err, result) {
+            if (err) console.log(err)
+        });    
+    }) ;res.redirect('./profile')   
 })
 
-// edit profile
-router.get('/edit', (req, res) => {
-    res.render('edit-profile.ejs')
+//fills in the edit page with the current info
+router.get('/edit',(req, res) =>{
+    var userList  = []
+    db.connect((err) => {
+        if (err) console.log(err)
+        console.log("Connected edit profile1!")
+        var sql = "SELECT User.uuid,User.first_name,User.last_name,User.email,User.password,User.permission_level from User WHERE User.email='omartest@test.com'";
+        db.query(sql, function(err, result) {
+            if (err) console.log(err)
+        
+            for (let i = 0; i < result.length; i++) {
+                //Sorts users based on role
+                userList.push(result[i])
+            }
+            res.render('edit-profile.ejs', {User: userList})
+        })    
+    })   
 })
-
 module.exports = router
