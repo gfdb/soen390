9522f3fa3eaedd2ee47189b17f49b669b97733c0
David Lemme <daviddantelemme@gmail.com>
2022-03-14 00:22:43 -0400

    added address to session and made edit profile page dynamic #120

--- routes/profile.js
+++ routes/profile.js
@@ -15,4 +15,16 @@ router.get('/edit', (req, res) => {
     res.render('edit-profile.ejs', { user: req.session.user, address: req.session.address })
 })
 
+router.post('/edit', (req, res) => {
+    var sql = "INSERT INTO User (uuid, first_name, last_Name, email, password, permission_level) VALUES (UUID(),'" +
+        user.name + "','" + user.lastname + "','" + user.email + "','" + hashedPassword + "','" + user.permissionLevel + "')"
+        // result/error handling
+    db.query(sql, function(err, result) {
+        if (err) console.log(err)
+        else
+            console.log("Number of records inserted: " + result)
+    })
+
+})
+
 module.exports = router
