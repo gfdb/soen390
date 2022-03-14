26bc83c4213d3008bc0d5524a3d23c49d433fb10
David Lemme <daviddantelemme@gmail.com>
2022-03-14 00:06:20 -0400

    added address to session and made profile dynamic #120

--- routes/login.js
+++ routes/login.js
@@ -3,6 +3,7 @@ const express = require('express')
 const router = express.Router()
 const db = require('../database')
 const User = require('../models/user')
+const Address = require('../models/address')
 const session = require('express-session')
 const store = new session.MemoryStore()
 const bcrypt = require('bcrypt')
@@ -19,20 +20,23 @@ router.use(session({
 // login post 
 router.post('/', (req, res) => {
     try {
-        db.query('SELECT * FROM User WHERE User.email = \'' + req.body.email + '\'', async(err, rows) => {
+        db.query('SELECT * FROM User,Address WHERE User.email = \'' + req.body.email + '\' AND User.uuid=Address.uuid', async(err, rows) => {
             try {
+
                 if (err) console.log(err)
                 if (rows.length == 0) throw new Error()
-                console.log(rows[0])
+                    // console.log(rows[0])
 
                 if (!await bcrypt.compare(req.body.password, rows[0].password)) throw new Error()
 
                 const user = new User(rows[0].uuid, rows[0].first_name, rows[0].last_name, rows[0].email, rows[0].permission_level)
-
+                const address = new Address(rows[0].uuid, rows[0].street_number, rows[0].street_name, rows[0].apartment_number, rows[0].city, rows[0].province, rows[0].zipcode)
                 req.session.authenticated = true
                 req.session.user = user
+                req.session.address = address
 
-                console.log(req.session.user)
+
+                // console.log(req.session.address)
                 req.session.save(() => { res.status(200).redirect('/profile') })
             } catch {
                 res.status(403).render("login_patient.ejs", { error: 'Invalid Credentials' })
--- routes/profile.js
+++ routes/profile.js
@@ -1,14 +1,18 @@
 const express = require('express')
 const router = express()
+const db = require('../database')
 
 // profile
 router.get('/', (req, res) => {
-    res.render('profile.ejs')
+
+    res.render('profile.ejs', { user: req.session.user, address: req.session.address })
+
 })
 
 // edit profile
 router.get('/edit', (req, res) => {
-    res.render('edit-profile.ejs')
+
+    res.render('edit-profile.ejs', { user: req.session.user, address: req.session.address })
 })
 
 module.exports = router
