4f39e6d99b97dc47d209cb635ec0ffd9b7daeced
gianfrancodumoulinbertucci <francodumoulin123@gmail.com>
2022-03-15 01:21:27 -0400

    fixed queries

--- server.js
+++ server.js
@@ -51,6 +51,14 @@ function checkNotAuthenticated(req, res, next) {
     next()
 }
 
+// check if admin
+function checkAdmin(req, res, next) {
+    if (req.session.authenticated && (req.session.user.permissionLevel.localeCompare('admin') === 0)) {
+        return next()
+    }
+    res.redirect('/')
+}
+
 // check if current user is a doctor
 function checkDoctor(req, res, next) {
     if (!req.session.authenticated) {
@@ -96,17 +104,6 @@ app.use('/messaging', checkAuthenticated, messagingRouter)
 // check if authenticated
 
 
-// check if not authenticated
-
-
-// check if admin
-function checkAdmin(req, res, next) {
-    if (eq.isAuthenticated() && (req.user.permissionLevel.localeCompare('admin') === 0)) {
-        return next()
-    }
-    res.redirect('/')
-}
-
 //added newly from front-end- probably need fixes on the backend
 app.get('/approveRoles', (req, res) => {
     var doctorList = []
@@ -117,7 +114,11 @@ app.get('/approveRoles', (req, res) => {
     db.connect((err) => {
         if (err) console.log(err)
         console.log("Connected!")
-        var sql = "SELECT Worker.role, User.first_name, User.last_name, Worker.user_uuid, User.email  FROM Worker, User WHERE Worker.user_uuid = User.uuid AND verified = 0";
+        var sql = `
+            SELECT Worker.role, User.first_name, User.last_name, Worker.user_uuid, User.email 
+            FROM Worker, User 
+            WHERE Worker.user_uuid = User.uuid 
+            AND verified = 0`;
         db.query(sql, function(err, result) {
             if (err) console.log(err)
 
@@ -189,9 +190,20 @@ app.get('/adminIndex', (req, res) => {
     res.render('admin_index.ejs')
 })
 
-app.get('/patientsAssign', (req, res) => {
+app.get('/patientsAssign', checkAdmin, (req, res) => {
     try {
-        db.query("SELECT * FROM User WHERE (permission_level = 'patient'OR permission_level='doctor') AND User.uuid NOT IN (SELECT Patient.user_uuid FROM Patient)", (err, result) => {
+
+        // select all patients who don't have a doctor 
+        // and all doctors
+        const sql = `
+            SELECT * 
+            FROM User
+            WHERE (permission_level = 'patient' OR (permission_level='doctor' AND User.uuid IN (SELECT Worker.user_uuid
+                                                                                                FROM Worker
+                                                                                                WHERE Worker.verified = 1))) 
+            AND User.uuid NOT IN (SELECT Doctor.patient_uuid 
+                                FROM Doctor)`
+        db.query(sql, (err, result) => {
             if (err) throw new Error()
             try {
                 let patients = []
@@ -228,9 +240,8 @@ app.post('/patientsAssign', (req, res) => {
     // console.log(req.body.patient_uuid)
 
 
-    const sql = "INSERT INTO Patient (user_uuid, doctor_uuid) VALUES ('" +
-        req.body.patient_uuid + "','" +
-        req.body.doctor + "')"
+    const sql = `INSERT INTO Doctor (user_uuid, patient_uuid) 
+        VALUES ('${req.body.doctor}', '${req.body.patient_uuid}')`
 
     db.query(sql, (err, result) => {
         if (err) console.log(err)
@@ -288,46 +299,6 @@ app.get('/doctorsPatientList', checkDoctor, (req, res) => {
             }
         }
         res.render('doctors_patient_list.ejs', { positivepatients: positivepatientList, negativepatients: negativepatientList })
-    })
-})
-
-
-app.post('/doctorsPatientProfile', checkDoctor, function(req, res) {
-    var user_uuid = req.body.uuid
-    var patientinfo = []
-        //Queries for the list of workers that have yet to be approved by the admin
-    var sql = "Select User.first_name, User.last_name, User.email,Patient.covid,Patient.symptoms,User.uuid FROM User,Patient Where User.uuid = '" + user_uuid + "' AND Patient.doctor_uuid = '" + req.session.user.uuid + "' AND permission_level = 'patient' AND User.uuid = Patient.user_uuid;";
-    db.query(sql, function(err, result) {
-        if (err) console.log(err)
-
-        //  if (result.length==0)
-        // {
-        //  throw "Error: No patient found when retrieving assigned patients for this doctor!"
-        // }
-        // else
-        // {
-
-
-        for (let i = 0; i < 1; i++) {
-
-            // covid = result[i].covid
-
-            //Sorts users based on role
-
-            patientinfo.push(result[i])
-
-
-            // case "health official":
-            //   healthOffList.push(result[i])
-            // break;
-            // case "immigration officer":
-            //   immigrationOffList.push(result[i])
-            // break;
-
-            //}   
-        }
-
-        res.render('doctors_patient_profile.ejs', { patientinfo: patientinfo })
     })
 })
 
@@ -446,124 +417,84 @@ app.post('/doctorMessaging', function(req, res) {
         
     });})}
 
-
-    //xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-
-
-
-
-
-
-    //console.log(patient_uuid)
+    const sql = `
+        SELECT * FROM (SELECT message.sender_uuid,message.receiver_uuid,message.message,message.first_name as senderFirstName, message.last_name AS senderLastName, message.date_time,receiver.first_name AS receiverFirstName, receiver.last_name AS receiverLastName
+                    FROM (Select sender_uuid,receiver_uuid,message,date_time,User.first_name,User.last_name 
+                            FROM Messages, User 
+                            WHERE sender_uuid = '${doctor_uuid}' 
+                            AND receiver_uuid = '${patient_uuid}' 
+                            AND sender_uuid = User.uuid 
+                            ORDER BY date_time DESC) As message, 
+                    (SELECT User.first_name,User.last_name,User.uuid 
+                        FROM User 
+                        WHERE User.uuid = '${patient_uuid}') AS receiver
+        WHERE message.receiver_uuid = receiver.uuid
+        UNION 
+        SELECT message.sender_uuid, message.receiver_uuid, message.message, message.first_name as senderFirstName, message.last_name AS senderLastName, message.date_time, receiver.first_name AS receiverFirstName, receiver.last_name AS receiverLastName 
+        FROM (Select sender_uuid, receiver_uuid, message, date_time, User.first_name, User.last_name
+            FROM Messages, User 
+            WHERE sender_uuid = '${patient_uuid}'
+            AND receiver_uuid = '${doctor_uuid}'
+            AND sender_uuid = User.uuid 
+            ORDER BY date_time DESC) AS message, 
+            (SELECT User.first_name, User.last_name, User.uuid
+            FROM User 
+            WHERE User.uuid = '${doctor_uuid}') AS receiver 
+        WHERE message.receiver_uuid = receiver.uuid ) X 
+        ORDER BY X.date_time ASC`
     
-
-    //Queries for the list of workers that have yet to be approved by the admin
-
-
-    //xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-
-    var sql = "SELECT * FROM ( SELECT message.sender_uuid,message.receiver_uuid,message.message,message.first_name as senderFirstName,message.last_name AS senderLastName, message.date_time,receiver.first_name AS receiverFirstName, receiver.last_name AS receiverLastName FROM "+
-
-
-    " (Select sender_uuid,receiver_uuid,message,date_time,User.first_name,User.last_name from Messages,User WHERE sender_uuid = '"+doctor_uuid+"' AND receiver_uuid = '"+patient_uuid+"' AND sender_uuid = User.uuid ORDER BY date_time DESC) As message, "+
-   
-    " (SELECT User.first_name,User.last_name,User.uuid FROM User WHERE User.uuid = '"+patient_uuid+"') AS receiver " +
-    
-   " WHERE message.receiver_uuid = receiver.uuid "+
-    
-" UNION " + 
-"SELECT message.sender_uuid,message.receiver_uuid,message.message,message.first_name as senderFirstName,message.last_name AS senderLastName, message.date_time,receiver.first_name AS receiverFirstName, receiver.last_name AS receiverLastName FROM "+
-
-
-  "  (Select sender_uuid,receiver_uuid,message,date_time,User.first_name,User.last_name from Messages,User WHERE sender_uuid = '"+patient_uuid+"' AND receiver_uuid = '"+doctor_uuid+"' AND sender_uuid = User.uuid ORDER BY date_time DESC) As message, "+
-   
-  "   (SELECT User.first_name,User.last_name,User.uuid FROM User WHERE User.uuid = '"+doctor_uuid+"') AS receiver " +
-    
-  "  WHERE message.receiver_uuid = receiver.uuid ) X ORDER BY X.date_time ASC";
-     //console.log(sql);
-    db.query(sql, function(err, result) {
-        if (err) console.log(err)
-
-        if (doctor_uuid == result[0].sender_uuid)
-        {
-            doctorFirstName = result[0].senderFirstName
-            doctorLastName = result[0].senderLastName
-            patientFirstName = result[0].receiverFirstName
-            patientLastName = result[0].receiverLastName
-        }
-        else if (patient_uuid == result[0].sender_uuid)
-        {
-            doctorFirstName = result[0].receiverFirstName
-            doctorLastName = result[0].receiverLastName
-            patientFirstName = result[0].senderFirstName
-            patientLastName = result[0].senderLastName
-        }
-        //console.log(result)
-        for (let i = 0; i < result.length; i++) {
-
-           
-                    messageList.push(result[i])
-                    
-            }
-           
-           res.render('doctor_messaging.ejs',{ doctor_uuid: doctor_uuid,patient_uuid: patient_uuid,patientFirstName:patientFirstName,patientLastName:patientLastName,messageList:messageList })
-        
-    })
-    
-   
- 
-   
-    
-})
-
-app.post('/sendMessage', function(req, res) {
-    let date_ob = new Date();
-
-    // current date
-    // adjust 0 before single digit date
-    let date = ("0" + date_ob.getDate()).slice(-2);
-
-    // current month
-    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
-
-    // current year
-    let year = date_ob.getFullYear();
-
-    // current hours
-    let hours = date_ob.getHours();
-
-    // current minutes
-    let minutes = date_ob.getMinutes();
-
-    // current seconds
-    let seconds = date_ob.getSeconds();
-
-   
-    
-    // console.log(req.body.messageList)
-    // console.log(doctor_uuid)
-    // console.log(patient_uuid)
-    // console.log(message)
-    // console.log(patientFirstName)
-    db.connect(function(err) {
-        if (err) throw err;
-        patient_uuid = req.body.patientuuid
-        doctor_uuid = req.session.user.uuid
-        message = req.body.doctormessage
-        patientFirstName = req.body.patientFirstName
-        patientLastName = req.body.patientLastName
-        messageList = req.body.messageList
-        console.log(messageList[0])
-        var sql = "INSERT INTO Messages  VALUES ('"+doctor_uuid+"','"+patient_uuid+"','"+message+"','"+year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds+"')";
         db.query(sql, function(err, result) {
-            if (err) throw err;
+            if (err) console.log(err)
+            patient_uuid = req.body.uuid
+            doctor_uuid = req.session.user.uuid
+            doctorFirstName = ""
+            doctorLastName =""
+            patientFirstName = ""
+            patientLastName = ""
            
-        
-        res.redirect('/doctorMessaging')
-    });
-    });
-    
+            if (result.length == 0)
+            {
+                console.log("I am inside empty message")
+                console.log(doctor_uuid)
+                console.log(patient_uuid)
+                var sql2 = "SELECT User.first_name,User.last_name FROM User WHERE User.uuid = '"+patient_uuid+"';"
+                db.query(sql2, function(err, result1) {
+                    if (err) console.log(err)
+                    patient_uuid = req.body.uuid
+                    doctor_uuid = req.session.user.uuid
+                    console.log(result1)
+                   
+                    patientFirstName = result1[0].first_name
+                    patientLastName = result1[0].last_name
+                
+                    res.render('doctor_messaging.ejs',{ doctor_uuid: doctor_uuid,patient_uuid: patient_uuid,patientFirstName:patientFirstName,patientLastName:patientLastName,messageList:messageList })
+                })
+            }
+            else
+            {
+                if (doctor_uuid == result[0].sender_uuid)
+                {
+                    doctorFirstName = result[0].senderFirstName
+                    doctorLastName = result[0].senderLastName
+                    patientFirstName = result[0].receiverFirstName
+                    patientLastName = result[0].receiverLastName
+                }
+                else if (patient_uuid == result[0].sender_uuid)
+                {
+                    doctorFirstName = result[0].receiverFirstName
+                    doctorLastName = result[0].receiverLastName
+                    patientFirstName = result[0].senderFirstName
+                    patientLastName = result[0].senderLastName
+                }
+                
+                for (let i = 0; i < result.length; i++)
+                    messageList.push(result[i])
+
+               res.render('doctor_messaging.ejs',{ doctor_uuid: doctor_uuid,patient_uuid: patient_uuid,patientFirstName:patientFirstName,patientLastName:patientLastName,messageList:messageList })
+            }
+        })
 })
+
 
 
 app.post('/patientMessaging', function(req, res) {
