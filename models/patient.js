864698cf9f49ad3f639ddbf6d10ac556d10357fe
david roper <davidroper321@gmail.com>
2022-03-16 18:24:42 -0400

    Documentation for JS models

--- models/patient.js
+++ models/patient.js
@@ -1,9 +1,14 @@
 module.exports = class Patient {
+    /**
+     * @param uuid {string} user id of patient
+     * @param covid {integer} covid status of patient (1 = covid positive, 0 = negative)
+     * @param symptoms {string} list of symptoms the patient has
+     * @param diary {string} the activities the patient partook in that day
+    */
     constructor(uuid, covid, symptoms, diary) {
         this.uuid = uuid
         this.covid = covid
         this.symptoms = symptoms
-        // this.address_uuid = address_uuid
         this.diary = diary
     }
 }
--- models/user.js
+++ models/user.js
@@ -1,10 +1,17 @@
 module.exports = class User {
+    //user class, constructor takes in uuid, name, lastname, email and permission level
+    /**
+     * @param uuid {string} user id
+     * @param name {string} name of user
+     * @param lastname {string}  lastname of user
+     * @param email {string} email of user
+     * @param permissionLevel {string} permission level of user (doctor, patient, healthcare worker, ...)
+    */
     constructor(uuid, name, lastname, email, permissionLevel) {
-        this.uuid = uuid
-        this.name = name;
-        this.lastname = lastname
-        // this.address_uuid = address_uuid
-        this.email = email
-        this.permissionLevel = permissionLevel
+        this.uuid = uuid 
+        this.name = name; 
+        this.lastname = lastname 
+        this.email = email 
+        this.permissionLevel = permissionLevel 
     }
 }
