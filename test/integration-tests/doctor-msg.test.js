96e157ad6a1e3b3f73f9feab6fcd51082b149d4a
David Lemme <daviddantelemme@gmail.com>
2022-03-16 22:01:48 -0400

    finished unit and integration test #138

--- test/integration-tests/doctor-msg.test.js
+++ test/integration-tests/doctor-msg.test.js
@@ -3,38 +3,36 @@ const supertest = require("supertest")
 const request = supertest(server_modules.app)
 const { describe } = require('mocha');
 const { expect } = require('chai');
-const Address = require('../../models/address');
+//integration test for login
 
-describe('Integration login to edit profile', () => {
-    context("edit profile", () => {
-        it('update Name', async() => {
+// happy path
+describe('Integration login doctor send message', () => {
+    context('doctor send message', () => {
+        it('send login', async() => {
+            const test = await request.get('/login')
+            expect(test.statusCode).to.equal(200)
+
+
             // make post request to login page with known 
             // valid user credentials
             const login = await request.post('/login')
                 .send({
-                    email: 'david@example.com',
-                    password: 'd'
+                    email: 'm@e.com',
+                    password: 'second'
                 })
                 .set("Content-Type", "application/x-www-form-urlencoded")
                 .type("form")
                 // expect to be redirected to profile
             expect(login.statusCode).to.equal(302)
-                //edititng profile 
-            const editName = await request.post('/profile/edit')
-                .send({
-                    firstName: 'David',
-                    lastName: 'Lemme',
-                    email: 'david@example.com',
-                    address: '12334',
-                    appartment: '#2',
-                    city: 'montreal',
-                    province: 'YT',
-                    zip: 'dddddd'
 
+            const msg = await request.post('/doctorMessaging/12728c1e-a40a-11ec-a042-0ee3f6edede7')
+                .send({
+                    doctormessage: 'thisisatest',
                 })
                 .set("Content-Type", "application/x-www-form-urlencoded")
                 .type("form")
-            expect(editName.statusCode).to.equals(302)
+                // expect to be redirected to profile
+            expect(msg.statusCode).to.equal(302)
 
         })
     })
--- test/integration-tests/patient-msg.test.js
+++ test/integration-tests/patient-msg.test.js
@@ -3,38 +3,36 @@ const supertest = require("supertest")
 const request = supertest(server_modules.app)
 const { describe } = require('mocha');
 const { expect } = require('chai');
-const Address = require('../../models/address');
+//integration test for login
 
-describe('Integration login to edit profile', () => {
-    context("edit profile", () => {
-        it('update Name', async() => {
+// happy path
+describe('Integration login send message', () => {
+    context('patient send message', () => {
+        it('send login', async() => {
+            const test = await request.get('/login')
+            expect(test.statusCode).to.equal(200)
+
+
             // make post request to login page with known 
             // valid user credentials
             const login = await request.post('/login')
                 .send({
-                    email: 'david@example.com',
-                    password: 'd'
+                    email: 'mm@ll.com',
+                    password: 'second'
                 })
                 .set("Content-Type", "application/x-www-form-urlencoded")
                 .type("form")
                 // expect to be redirected to profile
             expect(login.statusCode).to.equal(302)
-                //edititng profile 
-            const editName = await request.post('/profile/edit')
-                .send({
-                    firstName: 'David',
-                    lastName: 'Lemme',
-                    email: 'david@example.com',
-                    address: '12334',
-                    appartment: '#2',
-                    city: 'montreal',
-                    province: 'YT',
-                    zip: 'dddddd'
 
+            const msg = await request.post('/patientMessaging')
+                .send({
+                    patientmessage: 'thisisatest',
                 })
                 .set("Content-Type", "application/x-www-form-urlencoded")
                 .type("form")
-            expect(editName.statusCode).to.equals(302)
+                // expect to be redirected to profile
+            expect(msg.statusCode).to.equal(302)
 
         })
     })
