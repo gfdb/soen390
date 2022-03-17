bcdde5ba43dfdde6443c727476ed5f7463608c13
David Lemme <daviddantelemme@gmail.com>
2022-03-16 20:23:56 -0400

    added login fuincituon added integration text for edit progfile

---
+++ test/integration-tests/edit-profile.test.js
@@ -0,0 +1,41 @@
+const server_modules = require('../../server')
+const supertest = require("supertest")
+const request = supertest(server_modules.app)
+const { describe } = require('mocha');
+const { expect } = require('chai');
+const Address = require('../../models/address');
+
+describe('Integration main to login', () => {
+    context("edit profile", () => {
+        it('update Name', async() => {
+            // make post request to login page with known 
+            // valid user credentials
+            const login = await request.post('/login')
+                .send({
+                    email: 'david@example.com',
+                    password: 'd'
+                })
+                .set("Content-Type", "application/x-www-form-urlencoded")
+                .type("form")
+                // expect to be redirected to profile
+            expect(login.statusCode).to.equal(302)
+                //edititng profile 
+            const editName = await request.post('/profile/edit')
+                .send({
+                    firstName: 'David',
+                    lastName: 'Lemme',
+                    email: 'david@example.com',
+                    address: '12334',
+                    appartment: '#2',
+                    city: 'montreal',
+                    province: 'YT',
+                    zip: 'dddddd'
+
+                })
+                .set("Content-Type", "application/x-www-form-urlencoded")
+                .type("form")
+            expect(editName.statusCode).to.equals(302)
+
+        })
+    })
+})
--- test/teardown.test.js
+++ test/teardown.test.js
@@ -5,12 +5,9 @@ const { expect } = require('chai');
 
 
 // below code is for deleting the test user in test teardown
-db.connect((err) => {
+
+var sql = "DELETE FROM User WHERE (email = 'david@example.com')"
+db.query(sql, (err, result) => {
     if (err) console.log(err)
-        // console.log("Connected!")
-    var sql = "DELETE FROM User WHERE (email = 'test@user.com')"
-    db.query(sql, (err, result) => {
-        if (err) console.log(err)
-        else console.log('Rows affected' + result.affectedRows)
-    })
+    else console.log('Rows affected' + result.affectedRows)
 })
